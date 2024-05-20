10fbc94afe014eb4cbcd1b6761ed33613057eb8f
Миша <malorodovmihail009@gmail.com>
2024-05-20 16:35:53 +0300

    commit

--- app/src/main/java/com/example/practice11/MainActivity.java
+++ app/src/main/java/com/example/practice11/MainActivity.java
@@ -1,23 +1,45 @@
 package com.example.practice11;
 
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import androidx.appcompat.app.AppCompatActivity;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-
-    private WebView webView;
+    private MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        webView = (WebView) findViewById(R.id.webView);
-        webView.setWebViewClient(new WebViewClient()); //Помогает приложению открывать ссылки внутри WebView, а не во внешнем браузере
-        webView.getSettings().setJavaScriptEnabled(true); //Включаем поддержку JavaScript
 
-        webView.loadUrl("https://github.com/miha-mlv/practice10"); //Загрузка страницы
+        Button playButton = findViewById(R.id.platButton);
+        mediaPlayer = new MediaPlayer();
+        try {
+            mediaPlayer.setDataSource("URL вашего аудиофайла");
+                    mediaPlayer.prepare(); // Можно использовать prepareAsync() для сетевых потоков
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!mediaPlayer.isPlaying()) {
+                    mediaPlayer.start();
+                } else {
+                    mediaPlayer.pause();
+                }
+            }
+        });
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 }
